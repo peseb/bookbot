679f58d18037c34b1d2436c5c250c970f6751479
Petter Bjørkås <pbjorkaas@enua.no>
2025-05-29 06:56:43 +0200

    Char statistics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from stats import word_count
+from stats import char_count, word_count
 
 
 def get_book_text(filepath):
@@ -11,5 +11,8 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = word_count(text)
     print(f"{num_words} words found in the document")
+
+    char_stats = char_count(text)
+    print(char_stats)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(text: str):
     words = text.split()
     return len(words)
+
+def char_count(text: str):
+    characters = {}
+    for c in text:
+        lower = c.lower()
+        if lower in characters:
+            characters[lower]+=1
+        else:
+            characters[lower]=1
+    return characters
