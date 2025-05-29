0f87134d556d1521cda016214d4ba040de680d63
Petter Bjørkås <pbjorkaas@enua.no>
2025-05-29 06:48:59 +0200

    Read file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+    
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
